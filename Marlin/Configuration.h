0830a339d2b3ef61e9877532f7a02dda968f4159
Jorge Luis Serna Velasquez <jorgelserve@icloud.com>
2023-08-25 15:18:25 -0500

    todos los endstops funcionales, falta dual z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1052,7 +1052,7 @@
 //#define USE_WMAX_PLUG
 
 // Enable pullup for all endstops to prevent a floating state
-// #define ENDSTOPPULLUPS
+#define ENDSTOPPULLUPS
 #if DISABLED(ENDSTOPPULLUPS)
   // Disable ENDSTOPPULLUPS to set pullups individually
   //#define ENDSTOPPULLUP_XMIN
@@ -1077,7 +1077,7 @@
 #endif
 
 // Enable pulldown for all endstops to prevent a floating state
-#define ENDSTOPPULLDOWNS
+// #define ENDSTOPPULLDOWNS
 #if DISABLED(ENDSTOPPULLDOWNS)
   // Disable ENDSTOPPULLDOWNS to set pulldowns individually
   //#define ENDSTOPPULLDOWN_XMIN
